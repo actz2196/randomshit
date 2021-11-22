e886872a4bdecc4adbbcc8b4a6c3fbaa15c2b393
actz2196 <61994027+actz2196@users.noreply.github.com>
2021-11-22 18:23:34 -0500

    Delete Valiant.lua

--- Valiant.lua
+++
@@ -1,7 +0,0 @@
---join this group first tho https://www.roblox.com/groups/9994296/Valiant-Da-Hood-Script#!/about
-local plrGui = game:GetService("Players").LocalPlayer.PlayerGui
-local gui = Instance.new("ScreenGui",plrGui)
-gui.Name = "DaHood"
-local Frame = Instance.new("Frame",gui)
-Frame.Name = "yes"
-Frame.Size = UDim2.new(0,100,0,100)
