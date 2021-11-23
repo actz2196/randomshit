c6f90197a7be9afb9007f72da05cbce9d4724fa6
actz2196 <61994027+actz2196@users.noreply.github.com>
2021-11-22 21:46:56 -0500

    Update folder.lua

--- folder.lua
+++ folder.lua
@@ -27,6 +27,64 @@ function lib.Window()
                 TextButton.MouseButton1Click:Connect(function()
                     pcall(callback())
                 end)
+        end,
+        Folder = function(text)
+            local Folder = Instance.new("TextButton")
+            local FolderFrame = Instance.new("Frame")
+            local UIGridLayout = Instance.new("UIGridLayout")
+            local FolderFrameVal = Instance.new("IntValue")
+            Folder.Name = "Folder"
+            Folder.Parent = game.StarterGui.ActzLib.Frame
+            Folder.BackgroundColor3 = Color3.fromRGB(72, 72, 72)
+            Folder.Size = UDim2.new(0, 200, 0, 50)
+            Folder.Font = Enum.Font.SourceSans
+            Folder.Text = text
+            Folder.TextColor3 = Color3.fromRGB(255, 255, 255)
+            Folder.TextSize = 14.000
+            FolderFrameVal = 25
+            FolderFrameOpen = false
+            FolderFrame.Name = "FolderFrame"
+            FolderFrame.Parent = Folder
+            FolderFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            FolderFrame.BorderSizePixel = 0
+            FolderFrame.Position = UDim2.new(0, 0, 1, 0)
+            FolderFrame.Size = UDim2.new(0, 136, 0, 0)
+            UIGridLayout.Parent = FolderFrame
+            UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+            UIGridLayout.CellPadding = UDim2.new(0, 0, 0, 0)
+            UIGridLayout.CellSize = UDim2.new(1, 0, 0, 20)
+            Folder.MouseButton1Click:Connect(function()
+                    FolderFrameOpen = not FolderFrameOpen
+                    if FolderFrameOpen == true then
+                        FolderFrame.Size = UDim2.new(0,136,0,0)
+                        for i,v in pairs(FolderFrame:GetChildren()) do
+                            if v.ClassName ~= "UIGridLayout" then
+                                v.Visible = false
+                            end
+                        end
+                    elseif FolderFrameOpen == false then
+                        FolderFrame.Size = UDim2.new(0,136,0,FolderFrameVal)
+                        for  i,v in pairs(FolderFrame:GetChildren()) do
+                            if v.CLassName ~= "UIGridLayout" then
+                                v.Visible = true
+                            end
+                        end
+                    end
+            end)
+            return { 
+                Button = function(textz,callback)
+                     local TextButton = Instance.new("TextButton")
+                    TextButton.Parent = Frame
+                    TextButton.BackgroundColor3 = Color3.fromRGB(72, 72, 72)
+                    TextButton.Size = UDim2.new(0, 200, 0, 50)
+                    TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+                    TextButton.TextSize = 14.000
+                    TextButton.Text = textz
+                    TextButton.MouseButton1Click:Connect(function()
+                        pcall(callback())
+                    end)
+                end
+            }
         end
     }
 end
